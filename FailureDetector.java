2db2ccd842bf7fe9d39b2a27f16b283d358614b4
anubhavashok <anubhavashok93@gmail.com>
2013-04-12 20:13:37 -0500

    Update and rename FailureDetector.class to FailureDetector.java

--- FailureDetector.java
+++ FailureDetector.java
@@ -1,10 +1,14 @@
 public class failureDetector
 {
+
+  private ArrayList correctNodes=initNodes;
+
   public bool isAlive(Node p)
   {
     //ping and check for timeout
   }
-  private ArrayList correctNodes=initNodes;
+
+
   public  ArrayList correctNodes(ArrayList correctNodes)
   {
     for(Node p: correctNodes)
