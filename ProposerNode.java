cd4c0d8fc287a20250b64f9ea647e8dbf06ac27e
anubhavashok <anubhavashok93@gmail.com>
2013-04-12 20:43:53 -0500

    Update ProposerNode.java

--- ProposerNode.java
+++ ProposerNode.java
@@ -1,7 +1,8 @@
 public class Proposer extends Node
 {
-    private int propNum;
+    private int propNum;                
     private int numberOfTimesProposed=0;
+    private Object message;                                     //CHANGE OBJECT TYPE
     public void generateNewPropNum()
     {
         propNum=getId()+numberOfTimesProposed*Init.totalNodes;
@@ -10,5 +11,16 @@ public class Proposer extends Node
     public int getPropNum()
     {
         return propNum;
+    }
+    public void sendToQuorum(Proposal p)
+    {
+        //get list of correct nodes
+        //send proposal to all correct nodes
+    }
+    public void prepare()
+    {
+        generateNewPropNum();
+        Proposal prepareProposal = new Proposal(propNum,message);
+        sendToQuorum(prepareProposal);
     }
 }
