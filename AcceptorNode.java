d6cb5443b82f32903aee0b5af71996159f029e62
anubhavashok <anubhavashok93@gmail.com>
2013-04-12 21:51:27 -0500

    Update AcceptorNode.java

--- AcceptorNode.java
+++ AcceptorNode.java
@@ -1,4 +1,24 @@
 public class AcceptorNode extends Node
 {
+  private ArrayList<Proposal> receivedProposals;
+  private Proposal highestProposalAccepted;
   
+  
+  public void receivePrepare()
+  {
+    Proposal p = ;              // receive proposal
+    receivedProposals.add(p);
+  }
+  public generateHighestProposalAccepted()
+  {
+    Proposal currentHighest;
+    for(Proposal p: receivedProposals)
+    {
+      if(p.getPropNum()>currentHighest.getPropNum())
+      {
+        currentHighest=p;
+      }
+    }
+    highestProposalAccepted= currentHighest;
+  }
 }
