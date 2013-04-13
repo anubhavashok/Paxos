37099f287762cb953dc0c96f5c926e801a9ecc6e
anubhavashok <anubhavashok93@gmail.com>
2013-04-12 20:34:46 -0500

    Update ProposerNode.java

--- ProposerNode.java
+++ ProposerNode.java
@@ -1,10 +1,14 @@
 public class Proposer extends Node
 {
-    private int propNum=getId();
+    private int propNum;
+    private int numberOfTimesProposed=0;
+    public void generateNewPropNum()
+    {
+        propNum=getId()+numberOfTimesProposed*Init.totalNodes;
+        numberOfTimesProposed++ 
+    }
     public int getPropNum()
     {
-        int result = propNum;
-        propNum+=Init.totalNodes;
-        return result;
+        return propNum;
     }
 }
