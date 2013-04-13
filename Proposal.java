9590397d278c9aa7826ae8da7ee81335b40a348d
anubhavashok <anubhavashok93@gmail.com>
2013-04-12 20:37:48 -0500

    Update Proposal.java

--- Proposal.java
+++ Proposal.java
@@ -1,5 +1,19 @@
 public class Proposal
 {
   private int propNum;
+  private Object message;
   
+  Proposal(int propNum, Object message)
+  {
+    this.propNum=propNum;
+    this.message=message;
+  }
+  public int getPropNum()
+  {
+    return propNum;
+  }
+  public Object getMessage()
+  {
+    return message;
+  }
 }
