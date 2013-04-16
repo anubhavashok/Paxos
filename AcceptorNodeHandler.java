b19d8b422daac63f62f7f2c0ee45f26c20b622b2
anubhavashok <anubhavashok93@gmail.com>
2013-04-15 20:33:54 -0500

    Update AcceptorNodeHandler.java

--- AcceptorNodeHandler.java
+++ AcceptorNodeHandler.java
@@ -2,12 +2,12 @@ public class AcceptorNodeHandler
 {
   public void run(Node n)
   {
-    Proposer p = new Acceptor(n); //node takes on proposal role
+    AcceptorNode p = new AcceptorNode(n); //node takes on proposal role
     //--------------------------------------------------------------------//
     p.receivePrepare();                           //receive prepares
     p.generateHighestPrepareReceived();           //identify prepare with highest propNum
     p.sendPromise();                              //send promise to the nodes with propNum higher than previous
-    p.receiveAcceptRequest()                      //get all accept requests
+    p.receiveAcceptRequest();                     //get all accept requests
     p.sendAccept();                               //send accept to the proposal > highest accept request
   }
 }
