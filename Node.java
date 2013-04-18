c47efe7d614c20ebb2a0cebb1084aab5f5ea1e47
anubhavashok <anubhavashok93@gmail.com>
2013-04-17 23:10:57 -0500

    Update Node.java

--- Node.java
+++ Node.java
@@ -38,49 +38,7 @@ public class Node
 	{
 		return id;
 	}
-	public ServerSocket getServerSocket()			//Id accessor
-	{
-		return sSocket;
-	}
-	public void startServer()
-	{
-		try
-		{
-			ss=new ServerSocket(1234+id);
-			sSocket=ss.accept();
-			is =sSocket.getInputStream();
-			ois = new ObjectInputStream(is);
-		}
-		catch(Exception e)
-		{
-			System.out.println("server fault");
-		}
-	}
-	public void closeServer()
-	{
-		is.close();
-		sSocket.close();
-		ss.close();
-	}
-	public void closeClient()
-	{
-		oos.close();
-		os.close();
-		cSocket.close();
-	}
-	public void startClient()
-	{
-		try
-		{
-			cSocket= new Socket("localhost",1234+id);	//TCP socket
-			os=cSocket.getOutputStream();
-			oos = new ObjectOutputStream(os);
-		}
-		catch(Exception e)
-		{
-			System.out.println("client fault");
-		}
-	}
+
 	public InetAddress getInetAddress()
 	{
 		return s.getInetAddress();
